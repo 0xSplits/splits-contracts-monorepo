fa11844d051114c60addcf0f4d0c94a89e6439e1
Rooh Afza <coldroohafza@gmail.com>
2024-02-13 15:30:35 -0800

    address comment

--- packages/splits-v2/src/libraries/Math.sol
+++ packages/splits-v2/src/libraries/Math.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.23;
 
 library Math {
     function sum(uint256[] calldata values) internal pure returns (uint256 total) {
-        for (uint256 i = 0; i < values.length; ++i) {
+        for (uint256 i; i < values.length; ++i) {
             total += values[i];
         }
     }
 
     function sumMem(uint256[] memory values) internal pure returns (uint256 total) {
-        for (uint256 i = 0; i < values.length; ++i) {
+        for (uint256 i; i < values.length; ++i) {
             total += values[i];
         }
     }
