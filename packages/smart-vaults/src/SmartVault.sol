7cb619223cb672c44146c09fe02076dc72609fb7
Rooh Afza <coldroohafza@gmail.com>
2024-06-24 15:00:31 -0700

    feat: restructure smart vault

--- packages/smart-vaults/src/SmartVault.sol
+++ packages/smart-vaults/src/SmartVault.sol
@@ -14,110 +14,102 @@ import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
  * https://github.com/eth-infinitism/account-abstraction/blob/develop/contracts/samples/SimpleAccount.sol
  */
 contract SmartVault is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, Initializable {
-    address public owner;
+    /* -------------------------------------------------------------------------- */
+    /*                                   STORAGE                                  */
+    /* -------------------------------------------------------------------------- */
 
+    /// @notice Primary owner of the smart vault.
+    address public root;
+
+    /// @notice Entry point supported by the smart account.
     IEntryPoint private immutable _entryPoint;
 
-    event SimpleAccountInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+    /* -------------------------------------------------------------------------- */
+    /*                                   ERRORS                                   */
+    /* -------------------------------------------------------------------------- */
 
-    modifier onlyOwner() {
-        _onlyOwner();
+    error OnlyRoot();
+    error OnlyRootOrEntryPoint();
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   EVENTS                                   */
+    /* -------------------------------------------------------------------------- */
+
+    event SmartVaultInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  MODIFIERS                                 */
+    /* -------------------------------------------------------------------------- */
+
+    modifier onlyRoot() {
+        if (root != msg.sender) revert OnlyRoot();
         _;
     }
 
-    /// @inheritdoc BaseAccount
-    function entryPoint() public view virtual override returns (IEntryPoint) {
-        return _entryPoint;
+    modifier onlyRootOrEntryPoint() {
+        if (msg.sender != address(entryPoint()) && msg.sender != root) revert OnlyRootOrEntryPoint();
+        _;
     }
 
-    // solhint-disable-next-line no-empty-blocks
-    receive() external payable { }
+    /* -------------------------------------------------------------------------- */
+    /*                                   STRUCTS                                  */
+    /* -------------------------------------------------------------------------- */
+
+    /// @dev Call struct for the `executeBatch` function.
+    struct Call {
+        address target;
+        uint256 value;
+        bytes data;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
 
     constructor(IEntryPoint anEntryPoint) {
         _entryPoint = anEntryPoint;
         _disableInitializers();
     }
 
-    function _onlyOwner() internal view {
-        //directly from EOA owner, or through the account itself (which gets redirected through execute())
-        require(msg.sender == owner || msg.sender == address(this), "only owner");
-    }
+    /* -------------------------------------------------------------------------- */
+    /*                          EXTERNAL/PUBLIC FUNCTIONS                         */
+    /* -------------------------------------------------------------------------- */
 
-    /**
-     * execute a transaction (called directly from owner, or by entryPoint)
-     * @param dest destination address to call
-     * @param value the value to pass in this call
-     * @param func the calldata to pass in this call
-     */
-    function execute(address dest, uint256 value, bytes calldata func) external {
-        _requireFromEntryPointOrOwner();
-        _call(dest, value, func);
-    }
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable { }
 
-    /**
-     * execute a sequence of transactions
-     * @dev to reduce gas consumption for trivial case (no value), use a zero-length array to mean zero value
-     * @param dest an array of destination addresses
-     * @param value an array of values to pass to each call. can be zero-length for no-value calls
-     * @param func an array of calldata to pass to each call
-     */
-    function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external {
-        _requireFromEntryPointOrOwner();
-        require(dest.length == func.length && (value.length == 0 || value.length == func.length), "wrong array lengths");
-        if (value.length == 0) {
-            for (uint256 i = 0; i < dest.length; i++) {
-                _call(dest[i], 0, func[i]);
-            }
-        } else {
-            for (uint256 i = 0; i < dest.length; i++) {
-                _call(dest[i], value[i], func[i]);
-            }
-        }
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
     }
 
     /**
      * @dev The _entryPoint member is immutable, to reduce gas consumption.  To upgrade EntryPoint,
-     * a new implementation of SimpleAccount must be deployed with the new EntryPoint address, then upgrading
+     * a new implementation of SmartVault must be deployed with the new EntryPoint address, then upgrading
      * the implementation by calling `upgradeTo()`
-     * @param anOwner the owner (signer) of this account
+     * @param _owner the owner (signer) of this account
      */
-    function initialize(address anOwner) public virtual initializer {
-        _initialize(anOwner);
+    function initialize(address _owner) public virtual initializer {
+        _initialize(_owner);
     }
 
-    function _initialize(address anOwner) internal virtual {
-        owner = anOwner;
-        emit SimpleAccountInitialized(_entryPoint, owner);
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     * @param target target address to call
+     * @param value the value to pass in this call
+     * @param data the calldata to pass in this call
+     */
+    function execute(address target, uint256 value, bytes calldata data) external onlyRootOrEntryPoint {
+        _call(target, value, data);
     }
 
-    // Require the function call went through EntryPoint or owner
-    function _requireFromEntryPointOrOwner() internal view {
-        require(msg.sender == address(entryPoint()) || msg.sender == owner, "account: not Owner or EntryPoint");
-    }
-
-    /// implement template method of BaseAccount
-    function _validateSignature(
-        PackedUserOperation calldata userOp,
-        bytes32 userOpHash
-    )
-        internal
-        virtual
-        override
-        returns (uint256 validationData)
-    {
-        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
-        if (owner != ECDSA.recover(hash, userOp.signature)) {
-            return SIG_VALIDATION_FAILED;
-        }
-        return SIG_VALIDATION_SUCCESS;
-    }
-
-    function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory result) = target.call{ value: value }(data);
-        if (!success) {
-            assembly {
-                revert(add(result, 32), mload(result))
-            }
+    /**
+     * execute a sequence of transactions
+     * @param calls an array of calls to execute from this account
+     */
+    function executeBatch(Call[] calldata calls) external onlyRootOrEntryPoint {
+        for (uint256 i; i < calls.length; i++) {
+            _call(calls[i].target, calls[i].value, calls[i].data);
         }
     }
 
@@ -140,12 +132,46 @@ contract SmartVault is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, Initi
      * @param withdrawAddress target to send to
      * @param amount to withdraw
      */
-    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyOwner {
+    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyRoot {
         entryPoint().withdrawTo(withdrawAddress, amount);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal view override {
+    /* -------------------------------------------------------------------------- */
+    /*                             INTERNAL FUNCTIONS                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// implement template method of BaseAccount
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    )
+        internal
+        virtual
+        override
+        returns (uint256 validationData)
+    {
+        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        if (root != ECDSA.recover(hash, userOp.signature)) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{ value: value }(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal view override onlyRoot {
         (newImplementation);
-        _onlyOwner();
+    }
+
+    function _initialize(address _owner) internal virtual {
+        root = root;
+        emit SmartVaultInitialized(_entryPoint, _owner);
     }
 }
