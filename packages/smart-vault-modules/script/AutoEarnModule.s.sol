e8f8a680db6d34159b8f2ce4de254268a691aba0
Rooh Afza <coldroohafza@gmail.com>
2026-03-10 10:39:24 -0700

    chore(smart-vault-modules): hardcode USDC and Aave vault in deploy script

--- packages/smart-vault-modules/script/AutoEarnModule.s.sol
+++ packages/smart-vault-modules/script/AutoEarnModule.s.sol
@@ -6,12 +6,15 @@ import { AutoEarnModule } from "src/AutoEarnModule.sol";
 import { BaseScript } from "./Base.s.sol";
 
 contract AutoEarnModuleScript is BaseScript {
-    function run() public {
-        address usdc = getAddressFromConfig("usdc");
-        address vault = getAddressFromConfig("aaveUsdcVault");
+    /// @dev Base USDC address.
+    address constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 
+    /// @dev Base Aave USDC earn vault address.
+    address constant AAVE_VAULT = 0x4EA71A20e655794051D1eE8b6e4A3269B13ccaCc;
+
+    function run() public {
         vm.startBroadcast();
-        address module = address(new AutoEarnModule{ salt: keccak256("splits.autoEarnModule.v1") }(usdc, vault));
+        address module = address(new AutoEarnModule{ salt: keccak256("splits.autoEarnModule.v1") }(USDC, AAVE_VAULT));
         vm.stopBroadcast();
 
         updateDeployment(module, "AutoEarnModule");
